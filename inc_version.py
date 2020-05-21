50f86b17994ed0393c04c46242516b5df5cef2f7
Shimshon Fishler <shimshon.fishler@gmail.com>
2020-05-21 10:52:31 +0300

    change to get version from environment variable

--- inc_version.py
+++ inc_version.py
@@ -1,9 +1,9 @@
-import fileinput, re, sys
+import fileinput, re, sys, os
 
+version = os.getenv('RELEASE_VERSION')
 for line in fileinput.input(inplace=True):
-    m = re.search(r"#define MYAPP_VERSION (\d+)", line)
-    if m:
-        version = int(m.group(1))
-        sys.stdout.write(f"#define MYAPP_VERSION {version+1}\n")
+    m = re.search(r"#define MYAPP_VERSION ", line)
+    if m:        
+        sys.stdout.write(f"#define MYAPP_VERSION {version}\n")
     else:
         sys.stdout.write(line)
